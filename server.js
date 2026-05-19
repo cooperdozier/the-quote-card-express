07e2cf1b3aca16149ba16233f8eff2f3639228b4
cooperdozier <cooper.dozier@gmail.com>
2026-05-19 19:57:49 -0400

    express server to server a single page

---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = 7345;
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+    console.log("Press Ctrl-C to end thi process.");
+})
