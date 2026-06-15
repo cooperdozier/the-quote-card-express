33ad93e5681a5e81163cd90f6d41413453587226
cooperdozier <cooper.dozier@gmail.com>
2026-06-15 17:50:50 -0400

    part 3 done

--- public/js/index.js
+++ public/js/index.js
@@ -5,35 +5,54 @@ const elements = {
     author: document.getElementById("author"),
 }
 
-const quotes = [
-    {
-        quote: "All hands! Abandon ship!",
-        author: "Captain Picard"
-    },
-    {
-        quote: "Doh!",
-        author: "Homer Simpson",
-    },
-    {
-        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
-        author: "Eric Schmidt",
-    },
-    {
-        quote: "Do not worry, instead, do what springs immediately to hand",
-        author: "Cooper Dozier"
-    }
-]
 
-function loopThroughQuotes() {
-    let quoteIndex = 0;
-    setInterval(() => {
-        if (quoteIndex < quotes.length) {
-            elements.quote.textContent = quotes[quoteIndex].quote;
-            elements.author.textContent = quotes[quoteIndex].author;
-            quoteIndex++;
-        } else {
-            quoteIndex = 0;
-        }
-    }, 3000)
+async function getRandomImage() {
+    const client_id = "YOUR_KEY_HERE";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        console.log(returnedData)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
+getRandomImage();
+// const quotes = [
+//     {
+//         quote: "All hands! Abandon ship!",
+//         author: "Captain Picard"
+//     },
+//     {
+//         quote: "Doh!",
+//         author: "Homer Simpson",
+//     },
+//     {
+//         quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+//         author: "Eric Schmidt",
+//     },
+//     {
+//         quote: "Do not worry, instead, do what springs immediately to hand",
+//         author: "Cooper Dozier"
+//     }
+// ]
+
+// function loopThroughQuotes() {
+//     let quoteIndex = 0;
+//     setInterval(() => {
+//         if (quoteIndex < quotes.length) {
+//             elements.quote.textContent = quotes[quoteIndex].quote;
+//             elements.author.textContent = quotes[quoteIndex].author;
+//             quoteIndex++;
+//         } else {
+//             quoteIndex = 0;
+//         }
+//     }, 6000)
+// }
+
+// setTimeout(loopThroughQuotes, 6000);
